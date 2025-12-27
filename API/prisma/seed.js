230d67720debb8026cf43b666529f65490311bab
Valentin Neff <valentin.neff@ccicampus.fr>
2025-12-27 14:49:34 +0100

    🌱ajout seed creation utilisateur propriétaire

--- API/prisma/seed.js
+++ API/prisma/seed.js
@@ -1,1 +1,32 @@
-console.log('ok')
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const prisma = require("../constants/client.js");
+dotenv.config();
+
+const main = async () => {
+  const { OWNER_EMAIL, OWNER_PASSWORD, OWNER_NAME } = process.env;
+  const password = bcrypt.hashSync(OWNER_PASSWORD, 10);
+  const owner = {
+    name: OWNER_NAME,
+    password,
+    role: "ADMIN",
+  };
+  await prisma.user.upsert({
+    where: { email: OWNER_EMAIL },
+    update: owner,
+    create: {
+      ...owner,
+      email: OWNER_EMAIL,
+    },
+  });
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
