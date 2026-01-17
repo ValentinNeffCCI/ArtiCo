069d45dfe0f7157144921fa70e2a03bc85fffd81
Valentin Neff <valentin.neff@ccicampus.fr>
2026-01-17 18:45:36 +0100

    fix return

--- API/middlewares/error-middleware.js
+++ API/middlewares/error-middleware.js
@@ -10,8 +10,7 @@ module.exports = (err, req, res, next) => {
     if (err.status) {
         return res.status(err.status).json(err)
     }
-    res.status(500).json({
+    return res.status(500).json({
         error: (err.message || "Erreur serveur")
     })
-    next();
 }
